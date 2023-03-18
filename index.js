3de52345d426283c0d55e46104c278b686247aee
Aftermath001 <alvinasiachi@gmail.com>
2023-03-18 14:14:38 +0300

    Completed test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Write your code in this file!
+//const currentUser = 'Alvin Adams';
+
+//const welcomeMessage = 'Welcome to Flatbook, '+ currentUser;
+
+
+const currentUser = 'Alvin Adams';
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+ 
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+//const shortGreeting = `Welcome, ${currentUser}`;
+ 
+const shortGreeting = `Welcome, ${currentUser.slice(0, 1)}!`;
